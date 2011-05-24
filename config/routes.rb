05b11f58302daab0d3091eef352852c27dbc921d
Luis E. Guardiola <lguardiola@gmail.com>
2011-05-24 11:01:11 -0300

    fix routes drawer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-ApslabsOauthClient::Engine.routes.draw do
+Rails.application.routes.draw do
   # omniauth
   match '/auth/:provider/callback', :to => 'user_sessions#create'
   match '/auth/failure', :to => 'user_sessions#failure'
