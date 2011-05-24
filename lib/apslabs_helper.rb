d6e0ef510b8e73d9b3335ccb0183921c504ab081
Luis E. Guardiola <lguardiola@gmail.com>
2011-05-24 15:14:20 -0300

    import helpers methos to application scope

---
+++ lib/apslabs_helper.rb
@@ -0,0 +1,29 @@
+module ApslabsOauthClient
+  module Rails
+    module Helper
+      def current_user
+        return nil unless session[:user_id]
+        @current_user ||= User.find(session[:user_id])
+      end
+
+      def user_signed_in?
+        !!current_user
+      end
+
+      def login_required
+        unless user_signed_in?
+          respond_to do |format|
+            format.html {redirect_to '/auth/apslabs'}
+            format.json {render :json => { 'error' => 'Access Denied' }.to_json}
+          end
+        end
+      end
+
+      def current_company
+        @current_company ||= current_user.current_company || current_user.companies.first
+      end
+
+    end
+  end
+end
+
--- lib/engine.rb
+++ lib/engine.rb
@@ -1,7 +1,10 @@
 require 'omniauth'
+require 'apslabs_helper'
 
 module ApslabsOauthClient
   class Engine < Rails::Engine
+    ActionController::Base.helper ApslabsOauthClient::Rails::Helper
+
     initializer "omniauth" do |app|
       app.middleware.use OmniAuth::Builder do
         provider :apslabs, Rails.application.config.apslabs_federation_app_id, Rails.application.config.apslabs_federation_app_key, :site => Rails.application.config.apslabs_federation_url
