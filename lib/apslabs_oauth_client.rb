a446f529e7620cfd6a6f82f841c92a06838e63ac
Luis E. Guardiola <lguardiola@gmail.com>
2011-05-24 15:31:34 -0300

    Version bump to 0.5.1

--- lib/apslabs_oauth_client.rb
+++ lib/apslabs_oauth_client.rb
@@ -1,4 +1,4 @@
 module ApslabsOauthClient
-  require 'engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
+  require 'apslabs_oauth_client/engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
 end
 
---
+++ lib/apslabs_oauth_client/engine.rb
@@ -0,0 +1,14 @@
+require 'omniauth'
+
+module ApslabsOauthClient
+  class Engine < Rails::Engine
+    ActionController::Base.helper ApslabsOauthClient::Helper
+
+    initializer "omniauth" do |app|
+      app.middleware.use OmniAuth::Builder do
+        provider :apslabs, Rails.application.config.apslabs_federation_app_id, Rails.application.config.apslabs_federation_app_key, :site => Rails.application.config.apslabs_federation_url
+      end
+    end
+  end
+end
+
