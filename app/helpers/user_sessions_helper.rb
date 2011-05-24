ae06bdf7faca3f2a61ad8ce574bf22f600564969
Luis E. Guardiola <lguardiola@gmail.com>
2011-05-24 10:09:33 -0300

    add login helper

--- app/helpers/user_sessions_helper.rb
+++ app/helpers/user_sessions_helper.rb
@@ -2,11 +2,20 @@ module ApslabsOauthClient
   module UserSessionsHelper
     def current_user
       return nil unless session[:user_id]
-      @current_user ||= User.find_by_uid(session[:user_id]['uid'])
+      @current_user ||= User.find(session[:user_id])
     end
 
     def user_signed_in?
       !!current_user
+    end
+
+    def login_required
+      unless user_signed_in?
+        respond_to do |format|
+          format.html {redirect_to '/auth/apslabs'}
+          format.json {render :json => { 'error' => 'Access Denied' }.to_json}
+        end
+      end
     end
   end
 end
